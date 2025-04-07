a19764ab300a0a2938a730d7683ab7f37489af86
Kunal Hulke <hulke1102002@gmail.com>
2025-04-07 16:14:48 +0530

    `Added winston logging and updated dependencies`

---
+++ src/config/logger.ts
@@ -0,0 +1,41 @@
+import { Config } from './index';
+import winston from 'winston';
+
+const logger = winston.createLogger({
+    level: 'info',
+    defaultMeta: {
+        serviceName: 'auth-service',
+    },
+    transports: [
+        new winston.transports.File({
+            level: 'error',
+            dirname: 'logs',
+            filename: 'error.log',
+            format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.json(),
+            ),
+            silent: Config.NODE_ENV === 'TEST',
+        }),
+        new winston.transports.File({
+            level: 'info',
+            dirname: 'logs',
+            filename: 'combined.log',
+            format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.json(),
+            ),
+            silent: Config.NODE_ENV === 'TEST',
+        }),
+        new winston.transports.Console({
+            level: 'info',
+            format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.json(),
+            ),
+            silent: Config.NODE_ENV === 'TEST',
+        }),
+    ],
+});
+
+export default logger;
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
 import app from './app';
 import { Config } from './config';
+import logger from './config/logger';
 
 const startServer = () => {
     const PORT = Config.PORT;
     try {
         app.listen(PORT, () => {
-            // eslint-disable-next-line no-console
-            console.log(`Server is running on ${PORT}`);
+            logger.error('testing the error');
+            logger.info('Server listining on PORT', { port: PORT });
         });
     } catch (err) {
         // eslint-disable-next-line no-console
