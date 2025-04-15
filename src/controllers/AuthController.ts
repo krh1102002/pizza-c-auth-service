673a114813b6dbf9fe2fd0938f79438a6aa6cc53
Kunal Hulke <hulke1102002@gmail.com>
2025-04-15 14:33:07 +0530

    adding test cases to the register api....

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
 export class AuthController {
     register(req: Request, res: Response) {
-        res.status(201).send();
+        // res.status(201).send();  for the status code we write the beside code
+
+        res.status(201).json(); //we use want to share json data from the server to the client then we use .json() function to it
     }
 }
--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -1,4 +1,3 @@
-// import { request } from 'http';
 import app from './../../src/app';
 import request from 'supertest';
 describe('/auth/register', () => {
@@ -20,6 +19,31 @@ describe('/auth/register', () => {
 
             // Assert: check the result
             expect(res.statusCode).toBe(201);
+        });
+        it('should return valid json format:', async () => {
+            const userData = {
+                firstName: 'john',
+                lastName: 'doe',
+                email: 'john.doe@example.com',
+                password: 'securePassword123',
+            };
+            // Act: call the function or method being tested
+            const res = await request(app)
+                .post('/auth/register')
+                .send(userData);
+
+            // Assert: check the result
+            // we were using toEqual instead of toBe because we got object as a response from the response that's why we have to equate it to
+
+            // expect(res.headers['content-type']).toEqual(
+            //     expect.stringContaining('json'),
+            // );
+
+            // we don't know whether it is property or not so that's why we can't use above method for that we have to use below method..
+
+            expect(
+                (res.headers as Record<string, string>)['content-type'],
+            ).toEqual(expect.stringContaining('json'));
         });
     });
 
